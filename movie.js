e4ad38d847607aaa9ee6a84ef59b2c32fcb1fbaf
Henriquez <faith.henriquez@lathropgpm.com>
2025-10-16 13:18:44 -0700

    Version 4

--- movie.js
+++ movie.js
@@ -6,24 +6,42 @@
 
 const movieListEl = document.querySelector(".movie-list");
 
-document.body.classList = ' movies__loading';
-
 async function main () {
     const movies = await fetch ("http://www.omdbapi.com/?s=fast&apikey=fde10d57")
     const moviesData = await movies.json ();
     movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join ("");
-}
+    moviesWrapper.classList += ' movies__loading';
+
+    moviesWrapper.classList += ' movies__loading';
+
+  if (!movies) {
+    movies = await fetch ("http://www.omdbapi.com/?s=fast&apikey=fde10d57");
+  }
+  
+  moviesWrapper.classList.remove('movies__loading');
+} 
 main ();
         function movieHTML(movie) {
         return `<div class="movie-card">
            <div class="movie-card__container">
+           <img src="${movie.Poster}"></img>
               <h3>${movie.Title}</h4>
                 <p><b>Year:</b> ${movie.Year} </p>
-                <p><b>Poster:</b> <a href="https://${movie.Poster}" target="_blank">${movie.Poster}e</a></p>
+                
             </div>
           </div>`;
     
 }
+
+setTimeout(() => {
+  renderMovies();
+});
+
+function movies = await fetch ("http://www.omdbapi.com/?s=fast&apikey=fde10d57")
+    const moviesData = await movies.json () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([ 
 
 
 
