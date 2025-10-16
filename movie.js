74be3c352201f1538299721faba44b90ba049245
Henriquez <faith.henriquez@lathropgpm.com>
2025-10-16 09:58:36 -0700

    Version 3

--- movie.js
+++ movie.js
@@ -6,19 +6,24 @@
 
 const movieListEl = document.querySelector(".movie-list");
 
+document.body.classList = ' movies__loading';
+
 async function main () {
     const movies = await fetch ("http://www.omdbapi.com/?s=fast&apikey=fde10d57")
     const moviesData = await movies.json ();
     movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join ("");
 }
 main ();
-        function movieHTML(movies) {
+        function movieHTML(movie) {
         return `<div class="movie-card">
            <div class="movie-card__container">
-              <h3>${movies.title}</h4>
-                <p><b>Year:</b> ${movies.year} </p>
-                <p><b>Poster:</b> <a href="https://${movies.poster}" target="_blank">${movies.poster}e</a></p>
+              <h3>${movie.Title}</h4>
+                <p><b>Year:</b> ${movie.Year} </p>
+                <p><b>Poster:</b> <a href="https://${movie.Poster}" target="_blank">${movie.Poster}e</a></p>
             </div>
           </div>`;
     
-}s
+}
+
+
+
